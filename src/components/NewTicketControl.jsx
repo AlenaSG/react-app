150f73323360274282dff46398e6204c265b2b04
Alena Golovina <alenasgolovina@gmail.com>
2017-10-21 21:03:32 -0700

    add linter

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -14,11 +14,11 @@ class NewTicketControl extends React.Component {
     //this.handleDisplayingNewTicketForm = this.handleDisplayingNewTicketForm.bind(this);
   }
 
-  showFormModal(event) {
+  showFormModal() {
     this.setState({formModalIsShowing: true});
   }
 
-  hideFormModal(event) {
+  hideFormModal() {
     this.setState({formModalIsShowing: false});
   }
 
